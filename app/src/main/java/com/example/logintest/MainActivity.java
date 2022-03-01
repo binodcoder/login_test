afc47e145e5ea56e93349222ce1e641c13d7e035
binod <binodcoder@gmail.com>
2022-03-01 19:04:40 +0545

    login test

---
+++ app/src/main/java/com/example/logintest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.logintest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username=findViewById(R.id.et_login_username);
+        password=findViewById(R.id.et_login_password);
+        login=findViewById(R.id.btn_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().trim().equals("admin") && password.getText().toString().trim().equals("admin")){
+                    Intent intent=new Intent(MainActivity.this, HomePage.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "Invalid username and password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
